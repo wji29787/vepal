0bf3599b0b38476b348851ab54d4dcfaa26afa16
wuzhen <wji29787@163.com>
2018-10-11 14:24:52 +0800

    2018.10.11-14

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ import router from './router'
 import axios from 'axios'
 Vue.config.productionTip = false
 // 引入axios
-axios.defaults.baseURL = 'http://192.168.95.253:8085'
-// axios.defaults.baseURL = 'http://localhost:8080'
+// axios.defaults.baseURL = 'http://192.168.95.253:8085'
+axios.defaults.baseURL = 'http://localhost:8080'
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
-axios.defaults.headers['Content-Type'] = 'application/json;charset=UTF-8'
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+// axios.defaults.headers['Content-Type'] = 'application/json;charset=UTF-8'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
