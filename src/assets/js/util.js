224dbc2503aed79def28e6c5e0d59e07cc71317f
wuzhen <wji29787@163.com>
2018-10-18 11:00:27 +0800

    222

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -115,6 +115,30 @@ let filterNull = function filterNull(o) {
   }
   return o
 }
+// 获取窗口可视范围的高度
+function getClientHeight () {
+  var clientHeight = 0
+  if (document.body.clientHeight && document.documentElement.clientHeight) {
+    clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight
+  } else {
+    clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight
+  }
+  return clientHeight
+}
+// 获取窗口滚动条高度
+function getScrollTop () {
+  var scrollTop = 0
+  if (document.documentElement && document.documentElement.scrollTop) {
+    scrollTop = document.documentElement.scrollTop
+  } else if (document.body) {
+    scrollTop = document.body.scrollTop
+  }
+  return scrollTop
+}
+// 获取文档内容实际高度
+function getScrollHeight () {
+  return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight)
+}
 
 export {
   rect,
@@ -127,5 +151,9 @@ export {
   arc,
   text,
   toType,
-  filterNull
+  filterNull,
+  getClientHeight,
+  getScrollTop,
+  getScrollHeight
+
 }
--- src/vepal/vepal.vue
+++ src/vepal/vepal.vue
@@ -74,7 +74,8 @@ import 'ztree/css/zTreeStyle/zTreeStyle.css'
 import 'ztree/js/jquery.ztree.core.js'
 import 'ztree/js/jquery.ztree.excheck.js'
 import zrender from "zrender";
-import {
+import * as util from '../assets/js/util.js'
+import  {
   rect,
   rem,
   line,
@@ -116,30 +117,7 @@ let tlData = [
   }
 ];
 
-// 获取窗口可视范围的高度
-function getClientHeight(){  
-    var clientHeight=0;  
-    if(document.body.clientHeight&&document.documentElement.clientHeight){  
-        var clientHeight=(document.body.clientHeight<document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight;
-    }else{  
-        var clientHeight=(document.body.clientHeight>document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight;
-    }  
-    return clientHeight;  
-}
-// 获取窗口滚动条高度
-function getScrollTop(){  
-    var scrollTop=0;  
-    if(document.documentElement&&document.documentElement.scrollTop){  
-        scrollTop=document.documentElement.scrollTop;  
-    }else if(document.body){  
-        scrollTop=document.body.scrollTop;  
-    }  
-    return scrollTop;  
-}
-// 获取文档内容实际高度
-function getScrollHeight(){  
-    return Math.max(document.body.scrollHeight,document.documentElement.scrollHeight);  
-}
+
 export default {
   name: "Dinner",
   data() {
@@ -1195,11 +1173,11 @@ export default {
       let fn =function(e){
        
             // 窗口可视范围的高度
-        var height=getClientHeight(),
+        var height=util.getClientHeight(),
             // 窗口滚动条高度
-            theight=getScrollTop(),
+            theight=util.getScrollTop(),
             // 窗口可视范围的高度
-            rheight=getScrollHeight(),
+            rheight=util.getScrollHeight(),
             // 滚动条距离底部的高度
             bheight=rheight-theight-height;
             if(bheight<=100){
