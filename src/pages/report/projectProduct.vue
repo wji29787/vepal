fa1c133fb585fc9bfcb26cbbdfa256f5d8e4d913
xuchenmiao <1760000559@163.com>
2018-11-19 21:15:39 +0800

    x制作套装列表

--- src/pages/report/projectProduct.vue
+++ src/pages/report/projectProduct.vue
@@ -2,20 +2,36 @@
   <el-container class="r-project-product">
     <el-header class="r-project-product__header">
       <el-row :gutter="24">
-        <el-col :span="4">
-          <el-form-item label="项目名称">
-            <el-input v-model="searchParams.projectName"></el-input>
-          </el-form-item>
+        <el-col :span="6">
+          <el-row :gutter="24">
+            <el-col :span="7" style="padding-top:13px;">项目名称</el-col>
+            <el-col :span="17">
+              <el-select v-model="searchParams.projectName" filterable clearable>
+                <el-option v-for="(item, index) in selectProjectNameList"
+                          :key="index"
+                          :label="item.name"
+                          :value="item.projectId">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
         </el-col>
-        <el-col :span="4">
-          <el-form-item label="产品名称">
-            <el-input v-model="searchParams.productName"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="4">
-          <el-button type="primary">主要按钮</el-button>
+        <el-col :span="6">
+          <el-row :gutter="24">
+            <el-col :span="7" style="padding-top:13px;">产品名称</el-col>
+            <el-col :span="17">
+              <el-select v-model="searchParams.productName" filterable clearable>
+                <el-option v-for="(item, index) in productNameSelectList"
+                          :key="index"
+                          :label="item.productName"
+                          :value="item.productId">
+                </el-option>
+              </el-select>
+            </el-col>
+          </el-row>
         </el-col>
         <el-col :span="12">
+          <el-button type="primary" @click="handleSearchBtnClick">搜索</el-button>
           <el-button type="primary">导出</el-button>
         </el-col>
       </el-row>
@@ -27,30 +43,74 @@
         <el-table-column prop="address" label="地址"></el-table-column>
       </el-table>
     </el-main>
-    <el-footer class="r-project-product__footer"></el-footer>
+    <el-footer class="r-project-product__footer">
+      <el-pagination background
+        layout="prev, pager, next"
+        :total="totalNumber"
+        @current-change="handleCurrentPageChange">
+      </el-pagination>
+    </el-footer>
   </el-container>
 </template>
 <script>
 const GET_PROJECT_PRODUCT = '/api/suit/report/findProjectAndProductList';
+const PRODUCT_NAME_SELECT = '/api/pdc/product/findAllProduct';
+const PROJECT_NAME_SELECT = '/api/pjc/project/findAllProjectName';
 const RESPONSE_SUCCESS_CODE = 200;
 export default {
   name: 'projectProduct',
   data () {
     return {
       dataSource: [],
+      selectProjectNameList: [],
+      productNameSelectList: [],
       searchParams: {
         projectName: '',
         productName: '',
         currentPage: 1,
         limit: 10
       },
-      pageParams: {
-        pageSize: 10,
-        totalNumber: 0
-      }
+      totalNumber: 0
     };
   },
+  mounted () {
+    Promise.all([this.getProductNameList(), this.getPorjectNameList()]).then(res => {}).catch(err => {});
+  },
   methods: {
+    getProductNameList () {
+      return new Promise((resovle, reject) => {
+        let _data = {};
+        this.$http.post(PRODUCT_NAME_SELECT, _data, res => {
+          this.$nextTick(() => {
+            this.productNameSelectList = [];
+            if (res.data.code === RESPONSE_SUCCESS_CODE) {
+              let listData = res.data.data.list;
+              if (listData && Array.isArray(listData) && listData.length) {
+                this.productNameSelectList = listData;
+                console.log(listData, 'productList');
+              }
+            }
+          });
+        });
+      });
+    },
+    getPorjectNameList () {
+      return new Promise((resovle, reject) => {
+        let _data = {};
+        this.$http.post(PROJECT_NAME_SELECT, _data, res => {
+          this.$nextTick(() => {
+            this.selectProjectNameList = [];
+            if (res.data.code === RESPONSE_SUCCESS_CODE) {
+              let listData = res.data.data.list;
+              if (listData && Array.isArray(listData) && listData) {
+                this.selectProjectNameList = listData;
+                console.log(listData, 'projectList');
+              }
+            }
+          });
+        });
+      });
+    },
     getProjectProductList () {
       let _data = {
         pageNo: this.searchParams.currentPage,
@@ -61,11 +121,12 @@ export default {
       this.$http.get(GET_PROJECT_PRODUCT, _data, res => {
         this.$nextTick(() => {
           this.dataSource = [];
-          if (res.data.code === RESPONSE_SUCCESS_CODE) {
-          }
+          if (res.data.code === RESPONSE_SUCCESS_CODE) {}
         });
       });
-    }
+    },
+    handleSearchBtnClick () {},
+    handleCurrentPageChange (val) {}
   }
 };
 </script>
