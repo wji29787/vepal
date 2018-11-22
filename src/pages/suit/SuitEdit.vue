0b3aad6d8cdcf8d82d4b967c5b89393de58e2997
xuchenmiao <1760000559@163.com>
2018-11-22 11:55:47 +0800

    修改穿梭框排序

--- src/pages/suit/SuitEdit.vue
+++ src/pages/suit/SuitEdit.vue
@@ -577,6 +577,7 @@ export default {
       data.forEach((element, index) => {
         element.pid = 0;
         element.id = (index + 1).toString();
+        element.key = index + 1;
         element.label = element.name === null ? '' : element.name;
         element.children = element.products;
         delete element.products;
@@ -606,13 +607,13 @@ export default {
     compareData (params) {
       return function (obj1, obj2) {
         let val1 = obj1[params];
-        let val2 = obj2[params]; 
+        let val2 = obj2[params];
         if (val1 < val2 ) { //正序
+          return -1;
+        } else if (val1 > val2 ) {
           return 1;
-        } else if (val1 > val2 ) { 
-          return -1; 
-        } else { 
-          return 0; 
+        } else {
+          return 0;
         }
       }
     },
@@ -625,7 +626,7 @@ export default {
     // 监听穿梭框组件移除
     handleTransferRemove (fromData, toData, obj) {
       this.form.toData = toData;
-      this.projectList = fromData.sort(this.compareData('id'));
+      this.projectList = fromData.sort(this.compareData('key'));
       // 树形穿梭框模式transfer时，返回参数为左侧树移动后数据、右侧树移动后数据、移动的{keys,nodes,halfKeys,halfNodes}对象
       // 通讯录模式addressList时，返回参数为右侧收件人列表、右侧抄送人列表、右侧密送人列表
     }
